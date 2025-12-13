00fff3773c99cd898abd62c62206ed841252ebb5
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-12-13 16:14:05 +0530

    correcting PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const path = require("path")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
